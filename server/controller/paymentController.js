af47439f40ae532a014fba5d3c7f862fbfd818d8
Sanju tl <sanjutl19@gmail.com>
2025-05-27 16:36:56 +0530

    adearn 19

--- server/controller/paymentController.js
+++ server/controller/paymentController.js
@@ -8,7 +8,7 @@ const createPaymentIntent = async (req, res) => {
     try {
         const { userId } = req.params;
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: 2500, // 10 pounds (amount in pence)
+            amount: 100, // 1 pound (amount in pence)
             currency: 'gbp',
             payment_method_types: ['card'],
             metadata: {
@@ -17,7 +17,7 @@ const createPaymentIntent = async (req, res) => {
         });
         res.status(200).json({ clientSecret: paymentIntent.client_secret });
     } catch (error) {
-        res.status(500).json(   { error: error.message });
+        res.status(500).json({ error: error.message });
     }
 };
 
